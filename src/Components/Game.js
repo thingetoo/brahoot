1da7a21fe6d0140cd0f3f6a4841e0a22d166cf0b
Brandon Galloway <brandongalloway@Brandons-MBP.fios-router.home>
2021-01-09 21:54:42 -0500

    Added game logic for correct and incorrect answers and a score for the end of the game. Also added a button to restart the game

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -34,40 +34,87 @@ const Game = () => {
       questionText:
         "What are the three important manipulations done in a for loop on a loop variable in javascript?",
       answerOptions: [
-        { answerText: "the initialization, the Incrementation, and update" },
-        { answerText: "the initialization, the test, and the update" },
-        { answerText: "the initialization, the test, and Incrementation" },
-        { answerText: "All of the above" },
+        {
+          answerText: "the initialization, the Incrementation, and update",
+          isCorrect: false,
+        },
+        {
+          answerText: "the initialization, the test, and the update",
+          isCorrect: true,
+        },
+        {
+          answerText: "the initialization, the test, and Incrementation",
+          isCorrect: false,
+        },
+        { answerText: "All of the above", isCorrect: false },
       ],
     },
     {
       questionText:
         "What is the correct syntax for referring to an external script called 'LFC.js'?",
       answerOptions: [
-        { answerText: '<script src="LFC.js">' },
-        { answerText: '<script source="LFC.js">' },
-        { answerText: '<script ref="LFC.js">' },
-        { answerText: '<script type="LFC.js">' },
+        { answerText: '<script src="LFC.js">', isCorrect: true },
+        { answerText: '<script source="LFC.js">', isCorrect: false },
+        { answerText: '<script ref="LFC.js">', isCorrect: false },
+        { answerText: '<script type="LFC.js">', isCorrect: false },
       ],
     },
   ];
 
   const [questionNumber, setQuestionNumber] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [numberCorrect, setNumberCorrect] = useState(0);
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect) {
+      setNumberCorrect(numberCorrect + 1);
+    }
+    const nextQuestion = questionNumber + 1;
+    if (nextQuestion !== questions.length) {
+      setQuestionNumber(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const handleGameRestart = () => {
+    setShowScore(false);
+    setQuestionNumber(0);
+    setNumberCorrect(0);
+  };
 
   return (
     <div className="container">
       <div className="game-card">
-        <div className="question">
-          <span id="question-number">Question #{questionNumber + 1}</span>
-          <span id="question-text">
-            {questions[questionNumber].questionText}
-          </span>
-        </div>
-        <div className="answer-container">
-          {questions[questionNumber].answerOptions.map((answer) => (
-            <button className="each-answer-button">{answer.answerText}</button>
-          ))}
-        </div>
+        {showScore ? (
+          <div className="post-game">
+            <div className="correct">
+              You scored {numberCorrect} out of {questions.length}
+            </div>
+            <button className="restart" onClick={handleGameRestart}>
+              Play Again
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className="question">
+              <span id="question-number">Question #{questionNumber + 1}</span>
+              <span id="question-text">
+                {questions[questionNumber].questionText}
+              </span>
+            </div>
+            <div className="answer-container">
+              {questions[questionNumber].answerOptions.map((answer) => (
+                <button
+                  onClick={() => handleAnswerButtonClick(answer.isCorrect)}
+                  className="each-answer-button"
+                >
+                  {answer.answerText}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
