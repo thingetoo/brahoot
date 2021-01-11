a3730d05dbb9af91a571c959d6bb8a52c88cf6eb
Brandon Galloway <brandongalloway@Brandons-MBP.fios-router.home>
2021-01-10 21:52:40 -0500

    Added a high score component to the application that will track the top scores

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Game from "./Components/Game";
+import HighScores from "./Components/HighScores";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Game />
+      <HighScores />
     </div>
   );
 }
---
+++ src/Components/HighScores.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HighScores.css";
+
+const HighScores = () => (
+  <div>
+    <div className="hswordcontainer">
+      <span>Want to see where you rank?</span>
+      <span>Here are the top scores</span>
+    </div>
+  </div>
+);
+
+export default HighScores;
