3a2abb18091e90a9c3839be928c16aa445564f8c
Brandon Galloway <brandongalloway@Brandons-MBP.fios-router.home>
2021-01-14 16:00:40 -0500

    high scores are added more dynamically

--- src/Components/HighScores.js
+++ src/Components/HighScores.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
+import { questions } from "../data/Questions";
 import "./HighScores.css";
 
 const HighScores = (props) => {
   return (
-    <div>
-      <div className="hswordcontainer">
-        <span>Want to see where you rank?</span>
-        <span>Here are the top scores</span>
-        {props.userName ? props.userName : null}
-        {props.highScores.map((score) => {
-          return <p>{score}</p>;
+    <div className="hswordcontainer">
+      {/*<span>Want to see where you rank?</span>*/}
+      <span>Here are the top scores:</span>
+      <span>{props.userName ? `Current user: ${props.userName}` : null}</span>
+      <div className="high-score-values">
+        {props.highScores.map((score, idx) => {
+          return (
+            <div key={`highscore${idx}`} className="single-high-score">
+              <p>Attempt# {idx + 1}</p>
+              <p>
+                {score}/{questions.length}
+              </p>
+            </div>
+          );
         })}
       </div>
     </div>
