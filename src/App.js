c3ebf11c0cd4cb953cc019d8136b939999c174b5
Brandon Galloway <brandongalloway@Brandons-MBP.fios-router.home>
2021-01-21 23:39:17 -0500

    reorganized my page files and react-router code

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import "./App.css";
 
 import Header from "./Components/Header";
 import Game from "./Components/Game";
+import Home from "./Pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Game />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/about"></Route>
+
+          <Route path="/play"></Route>
+          <Route path="/sign-in"></Route>
+          <Route path="/sign-up"></Route>
+        </Switch>
+        <Header />
+        <Game />
+      </div>
+    </Router>
   );
 }
 
